2a08d3cad5c97649734276e9b9baf4f70736bc71
jinlow <james.d.inlow@gmail.com>
2019-09-08 21:01:21 -0500

    Document scorex

--- R/scorex.R
+++ R/scorex.R
@@ -2,11 +2,35 @@
 #'
 #' A dual score matrix function that allows for multiple
 #' performance, and cross variables.
+#'
+#' @param formula A formula of the form LHS ~ RHS to use to specify which fields
+#'   to use as the scores and performance measures in the tables, see Details.
+#' @param data A data.frame, that has the fields specified in the formula.
+#' @param cut_method The method to use to format the score in the final table.
+#'   Currently the available methods are:
+#'   \itemize{
+#'     \item \code{bins}: Break the scores into a set number of even bins.
+#'     \item \code{breaks}: Set the score values to break the score at.
+#'     \item \code{percentiles}: Set the percentiles to use to break the scores at.
+#'   }
+#' @param method_args Arguments to use for the method specified in \code{cut_method}
+#'   each method uses the following argument.
+#'   \itemize{
+#'     \item \code{bins}: Provide a integer number of breaks to use.
+#'     \item \code{breaks}: Provide a vector that specifies the cuts to use.
+#'     \item \code{percentiles}: Provide a vector of percentiles between the values of 0 and 1.
+#'   }
+#' @param exceptions A vector of exception values to exclude from the cross table.
+#'   The default is set to \code{NA}.
+#' @param custom_cut_fnc A custom function to use to cut the scores.
+#' @param ... Other arguments to pass to the \code{custom_cut_fnc}
+#' @details
+#' Fill out...
 #' @export
 scorex <- function(formula, data,
                    cut_method = c("bins", "breaks", "percentiles"),
                    method_args = NULL,
-                   exceptions = NULL,
+                   exceptions = NA,
                    custom_cut_fnc = NULL, ...) {
   # Get data fields
   fd <- get_formula_fields(formula, data)
